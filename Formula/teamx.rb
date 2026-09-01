114443cc81ea93ff45d558359fe7a305b66f1aee
caishu <booker.cai97@gmail.com>
2026-09-01 09:15:32 +0800

    teamx v0.2.0: fill in sha256 + fix tarball dir handling

--- Formula/teamx.rb
+++ Formula/teamx.rb
@@ -10,23 +10,26 @@ class Teamx < Formula
   on_macos do
     on_arm do
       url "https://github.com/caishu97/teamx-enterprise/releases/download/v0.2.0/teamx-macos-arm64.tar.gz"
-      sha256 "REPLACE_MACOS_ARM64_SHA"
+      sha256 "a4c0647d001864146afde89684a73fc0862706e84f37234364421ec7f9038124"
     end
   end
 
   on_linux do
     on_x86_64 do
       url "https://github.com/caishu97/teamx-enterprise/releases/download/v0.2.0/teamx-linux-x86_64.tar.gz"
-      sha256 "REPLACE_LINUX_X64_SHA"
+      sha256 "79eca89ff307ea827f96e4bd840d46704f2beb78a91bc137f654cf6cb191268f"
     end
     on_arm do
       url "https://github.com/caishu97/teamx-enterprise/releases/download/v0.2.0/teamx-linux-arm64.tar.gz"
-      sha256 "REPLACE_LINUX_ARM64_SHA"
+      sha256 "a63f8be838760393a1bb58fd6aa4db4010afb9e717cf6f25774950799c362c15"
     end
   end
 
   def install
-    bin.install "bin/teamx"
+    # The tarball wraps everything in a teamx-<version>/ directory.
+    cd "teamx-#{version}" do
+      bin.install "bin/teamx"
+    end
   end
 
   test do
